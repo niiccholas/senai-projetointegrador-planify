4168166ba9c427149cb7d2d7ad55fa9ac011f0be
Nicolas <24122316@CFP127ALUNO.LOCAL>
2025-05-22 14:32:11 -0300

    dao usuario finalizada

---
+++ app.js
@@ -0,0 +1,20 @@
+/****************************************************************************
+ * Objetivo: API responsável pelas requisições do projeto de gestão de eventos
+ * Data: 13/05/2025
+ * Autor: Nicolas
+ * Versão: 1.0
+ * Observações: Para criar a API precisamos instalar:
+ *      express     npm install express --save
+ *      cors       npm install cors --save
+ *      body-parser     npm install body-parser --save
+ * 
+ * Para criar a conexão com o banco de dados MYSQL precisamos instalar:
+ *      prisma      npm install prisma --save
+ *      prisma/client       npm install @prisma/client --save
+ * 
+ * Após a instalação do prisma é necessário inicializar o prisma:
+ *      npx prisma init
+ * 
+ * Para sincronizar o prisma com o banco de dados podemos utilizar:
+ *      npx prisma migrate dev
+*****************************************************************************/
