f6284852958661cd3b2b3291e7123b62847e4347
NICOLAS SILVA DE ALMEIDA SANTOS <24122316@CFP127ALUNO.LOCAL>
2025-05-22 13:56:27 -0300

    dao usuario iniciada

---
+++ model/usuario.js
@@ -0,0 +1,31 @@
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+const insertUser = async function(){
+
+}
+
+const selectUser = async function(){
+
+}
+
+const searchUser = async function(){
+
+}
+
+const updateUser = async function(){
+
+}
+
+const deleteUser = async function(){
+
+}
+
+module.exports = {
+    insertUser,
+    searchUser,
+    selectUser,
+    updateUser,
+    deleteUser
+}
